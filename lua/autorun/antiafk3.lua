e28b2f61bff12b4a68676aadb87353ebce49bcc2
Cheezus <rcheeze1337@gmail.com>
2020-01-19 14:45:24 -0500

    Cleanup & misc fixes

--- lua/autorun/antiafk3.lua
+++ lua/autorun/antiafk3.lua
@@ -28,31 +28,33 @@ else
     local function stopAFK(ply)
         if not IsValid(ply) then return end
 
-        if ply.afkTime > GetConVar("antiafk_afktime"):GetInt() * 60 then
-            print("[AAFK] " .. ply:Nick() .. " is no longer AFK!")
+        if ply.afkTime > GetConVar("aafk_afktime"):GetInt() * 60 then
+            print(ply:Nick() .. " is no longer AFK!")
             sendmsg(nil, Color(255, 0, 255), "[Server] ", Color(255, 255, 100), ply:Nick(), Color(255, 255, 255), " is no longer AFK!")
         end
 
         ply.afkTime = 0
-
-        if not ply.hasMovedAfterSpawning then
-            ply.hasMovedAfterSpawning = true
-        end
+        ply.hasMovedAfterSpawning = true
     end
 
     hook.Add("PlayerInitialSpawn", "antiafk_InitPlayer", function(ply)
         ply.afkTime = 0
     end)
 
+    if timer.Exists("antiafk_AfkClock") then
+        timer.Remove("antiafk_AfkClock")
+    end
+
     timer.Create("antiafk_AfkClock", 1, 0, function()
         local afkTime = GetConVar("aafk_afktime"):GetInt() * 60
+        if afkTime <= 0 then return end
         local kickTime = GetConVar("aafk_kicktime"):GetInt() * 60
         local spawnKickTime = GetConVar("aafk_spawnkicktime"):GetInt() * 60
         local excludeAdmins = GetConVar("aafk_excludeadmins"):GetBool()
         local onlyWhenFull = GetConVar("aafk_onlywhenfull"):GetBool()
-        if afkTime <= 0 then return end
+
         if kickTime <= afkTime then
-            GetConVar("antiafk_kicktime"):SetInt(afkTime + 1)
+            GetConVar("aafk_kicktime"):SetInt(afkTime + 1)
         end
 
         for _, ply in pairs(player.GetAll()) do
@@ -64,21 +66,23 @@ else
                     sendmsg(nil, Color(255, 0, 255), "[Server] ", Color(255, 255, 100), ply:Nick(), Color(255, 255, 255), " is now AFK!")
                 end
 
-                if (ply:IsAdmin() and excludeAdmins) or onlyWhenFull then break end
+                if ply:IsAdmin() and excludeAdmins then break end
+                if player.GetCount() < game.MaxPlayers() and onlyWhenFull then break end
 
                 if spawnKickTime > 0 and ply.afkTime >= spawnKickTime and not ply.hasMovedAfterSpawning then
-                    sendmsg(nil, Color(255, 0, 255), "[Server] ", Color(255, 255, 100), ply:Nick(), Color(255, 255, 255), " has been kicked for being AFK more than " .. math.floor(spawnKickTime / 60) .. " minutes after spawning.")
-                    ply:Kick("Kicked for being AFK more than 5 minutes after spawning.")
+                    sendmsg(nil, Color(255, 0, 255), "[Server] ", Color(255, 255, 100), ply:Nick(), Color(255, 255, 255), " has been kicked for being AFK more than " .. (spawnKickTime / 60) .. " minutes after spawning.")
+                    ply:Kick("Kicked for being AFK more than 5 minutes after spawning")
                 end
 
-                if kickTime > 0 and ply.afkTime >= kickTime then
+                if ply.afkTime >= kickTime then
                     ply.afkTime = 0
-                    sendmsg(nil, Color(255, 0, 255), "[Server] ", Color(255, 255, 100), ply:Nick(), Color(255, 255, 255), " has been kicked for being AFK more than " .. math.floor(kickTime / 60) .. " minutes.")
-                    ply:Kick("Kicked for being AFK more than " .. math.floor(kickTime / 60) .. " minutes.")
+                    sendmsg(nil, Color(255, 0, 255), "[Server] ", Color(255, 255, 100), ply:Nick(), Color(255, 255, 255), " has been kicked for being AFK more than " .. (kickTime / 60) .. " minutes.")
+                    ply:Kick("Kicked for being AFK more than " .. (kickTime / 60) .. " minutes")
                 end
 
-                if kickTime > 0 and ply.afkTime == kickTime - 300 then
+                if ply.afkTime == kickTime - 300 then
                     sendmsg(ply, Color(255, 0, 0), "Warning: You will be kicked soon if you remain inactive.")
+                    ply:SendLua("system.FlashWindow()") --Attempt to get the players attention
                 end
             end
         end
